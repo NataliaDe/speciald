3080e918ae4fe1cd2751f9dbfa21da977736beed
Natalia <deschenatalia@gmail.com>
2020-06-17 15:04:28 +0300

    импорт должностей

---
+++ DBSql/17-06-2020 ALTER_settings.sql
@@ -0,0 +1,4 @@
+INSERT INTO `speciald`.`settings` (`name`, `type`) VALUES ('ДОЛ. по умолчанию', 'is_dolj_add');
+
+INSERT INTO `speciald`.`settings_options` (`id_settings`, `name`, `option`) VALUES ('3', 'Выкл.', 'no');
+INSERT INTO `speciald`.`settings_options` (`id_settings`, `name`, `option`, `is_default`) VALUES ('3', 'Вкл.', 'yes',0);
